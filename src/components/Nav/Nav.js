951c23c422a6c8910702a1bedff8e4afbeeefde1
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-03-06 22:10:18 +0900

    Add: 초기 세팅 피드백 반영

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Nav.scss';
 
 const Nav = () => {
-  return <dic>Nav</dic>;
+  return (
+    <nav>
+      <img src="/images/Nav/로고 이미지.png" />
+    </nav>
+  );
 };
 
 export default Nav;
