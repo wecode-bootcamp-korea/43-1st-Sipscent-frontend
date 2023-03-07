2d3f80cae09a52cafb4f661765f2e9069ccc59b1
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-07 19:48:16 +0900

    ADD: 메인 레이아웃 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,21 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { MAIN_CATEGORY } from './mainCategoryData';
 import './Main.scss';
 
 const Main = () => {
-  const [scentImg, setScentImg] = useState([]);
+  // const [carousel, setCarousel] = useState();
+  // const [scentImg, setScentImg] = useState([]);
 
-  fetch('http://10.58.52.201:8002/test')
-    .then(res => res.json())
-    .then(data => {
-      setScentImg(data);
-    });
-  console.log(setScentImg);
+  // fetch('http://10.58.52.201:8002/test')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     setScentImg(data);
+  //   });
+  // console.log(setScentImg);
+
+  // useEffect(() => {
+  //   fetch('/data/mainCarouselBox.json', {
+  //     method: 'GET',
+  //   })
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setCarousel(data);
+  //     });
+  // }, []);
+
   return (
     <div className="main">
       <section className="sectionInfo">
         <div className="sectionInfoBox">
           <div className="sectionInfoContents">
-            <h1>감각적인 실내 공간을 위한 홈 프래그런스{scentImg}​</h1>
+            <h1>감각적인 실내 공간을 위한 홈 프래그런스​</h1>
             <p>
               분위기 전환이 필요한 가정이나 사무실에 이상적인 이솝의 홈 레인지는
               아로마틱 인센스, 룸 스프레이 등 다양한 방향 제품으로 구성되어
@@ -26,16 +39,33 @@ const Main = () => {
             More {'>'}
           </button>
         </div>
-        <img className="sectionInfoImage" src="/images/Main/Main.png" />
+        <img className="sectionInfoImage" src="/images/Main/main.png" />
       </section>
       <section className="sectionCarousel">
-        <button>왼쪽버튼</button>
-        <div className="map">
-          <div className="sectionCarouselImageBox" />
-          <div className="sectionCarouselImage" />
-          <div />
+        <div className="sectionCarouselButtonBox">
+          <button className="sectionCarouselButton Left">{'<'}</button>
+          <button className="sectionCarouselButton Right">{'>'}</button>
         </div>
-        <button>오른쪽버튼</button>
+        <ul className="sectionCarouselList">
+          <li className="sectionCarouselInfoBox">
+            <h1 className="sectionCarouselInfoBoxName">향기의 즐거움</h1>
+            <p className="sectionCarouselInfoBoxParagraph">
+              향수는 개발하는 동안 느꼈던 즐거움을 고객들이 사용하면서 만끽할 수
+              있기를 바라며 만들어졌습니다. 각각의 향수는 플로럴, 프레쉬, 우디,
+              오퓰런트 계열로 나뉘며, 경우에 따라 두 가지에 속하기도 합니다.
+            </p>
+          </li>
+          {MAIN_CATEGORY &&
+            MAIN_CATEGORY.map((info, key) => (
+              <li key={key} className="sectionCarouselImageBox">
+                <img className="sectionCarouselImage" src={info.image} />
+                <div className="sectionCarouselImageInfo">
+                  <h2 className="sectionCarouselImageName">{info.name}</h2>
+                  <p className="sectionCarouselImageParagraph">{info.text}</p>
+                </div>
+              </li>
+            ))}
+        </ul>
       </section>
     </div>
   );
---
+++ src/pages/Main/mainCategoryData.js
@@ -0,0 +1,38 @@
+export const MAIN_CATEGORY = [
+  {
+    id: 0,
+    name: 'Floral',
+    text: '플로롤로롤한 향기',
+    image: '/images/Main/MainFloral.jpg',
+  },
+  {
+    id: 1,
+    name: 'Citrus',
+    text: '시틀시틀한 향기',
+    image: '/images/Main/MainCitrus.jpg',
+  },
+  {
+    id: 2,
+    name: 'Herbal',
+    text: '허부르를한 향기',
+    image: '/images/Main/MainHerbal.jpg',
+  },
+  {
+    id: 3,
+    name: 'Spicy',
+    text: '매운 향기',
+    image: '/images/Main/MainSpicy.jpg',
+  },
+  {
+    id: 4,
+    name: 'Fruity',
+    text: '프룻프룻한 향기',
+    image: '/images/Main/MainFruity.jpg',
+  },
+  {
+    id: 5,
+    name: 'Green',
+    text: '초록초록한 향기',
+    image: '/images/Main/MainGreen.jpg',
+  },
+];
