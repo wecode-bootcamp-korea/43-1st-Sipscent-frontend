c974bac3fd2226376b4b97deef25243cb8718a78
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-03-08 20:15:44 +0900

    Add: product 이미지 크기 조절

--- src/pages/ProductList/ProductWrap.js
+++ src/pages/ProductList/ProductWrap.js
@@ -16,10 +16,21 @@ const ProductWrap = ({ key, img, name, scent, amount, price }) => {
       }}
     >
       <Link className="productInfo" to="/productdetail">
-        <img className="teaBagImg" src={img} alt="teabag" />
+        <div className="teaBagImgWrap">
+          <div
+            className="teaBagImg"
+            style={{ backgroundImage: `url(${img})` }}
+          />
+        </div>
         <h3 className="teaName">{name}</h3>
-        <p className="teaScent">{scent}</p>
-        <p className="teaAmount">{amount}</p>
+        <p className="teaScent">
+          <span className="teaScentTitle">향</span>
+          <span>{scent}</span>
+        </p>
+        <p className="teaAmount">
+          <span className="teaAmountTitle">용량</span>
+          <span>{amount}</span>
+        </p>
       </Link>
       {isHover ? (
         <button className="addCartButton">{`카트에 추가 - ${price}`}</button>
