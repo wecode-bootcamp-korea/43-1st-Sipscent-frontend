8297b91472b27ebb92b8798b6dfd22a07af049b4
김민경 <gimmingyeong@gimmingyeong-ui-MacBookPro.local>
2023-03-09 11:52:35 +0900

    ADD:회원가입 기능 구현

---
+++ src/pages/Signup/Chkbox.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import './Chkbox.scss';
+
+const Chkbox = ({ key, head }) => {
+  const [chkBx, setChkBx] = useState(true);
+  const isChkBxClick = () => {
+    setChkBx(!chkBx);
+  };
+
+  return (
+    <div key={key}>
+      <input onClick={isChkBxClick} type="checkbox" />
+
+      <span>{head}</span>
+      <p className={`chkOrNot ${chkBx ? 'block' : 'show'}`}>
+        체크 여부를 확인해주세요^^{' '}
+      </p>
+      <div className="useMandatory">{head}</div>
+    </div>
+  );
+};
+
+export default Chkbox;
