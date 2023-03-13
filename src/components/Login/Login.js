62ea3090e27176bc635281caaff2a7331382d1d6
김민경 <gimmingyeong@gimmingyeong-ui-MacBookPro.local>
2023-03-13 10:08:46 +0900

    PR modified

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,27 +6,28 @@ const Login = ({ setModalOpen }) => {
   const closeModal = () => {
     setModalOpen(false);
   };
-  //입력한 pw 볼 수 있는 state
+
   const [showPw, setShowPw] = useState(false);
-  const [userInfo, setUserInfo] = useState({ userId: '', userPw: '' });
+  const [userInfo, setUserInfo] = useState({ email: '', password: '' });
 
   function getUserInfo(e) {
     const { name, value } = e.target;
     setUserInfo({ ...userInfo, [name]: value });
   }
+  console.log(userInfo);
   //이메일 밑에 p 태그
   const emailChk =
-    (userInfo.userId.includes('@') && userInfo.userId.includes('.')) ||
-    !userInfo.userId;
-  console.log(emailChk);
+    (userInfo.email.includes('@') && userInfo.email.includes('.')) ||
+    !userInfo.email;
+  // console.log(emailChk);
   //비밀번호 밑에 p 태그
-  const pwChk = userInfo.userPw.length > 5 || !userInfo.userPw;
+  const pwChk = userInfo.password.length > 5 || !userInfo.password;
 
   //링크 넘어가게
   const isUserVaidate =
-    userInfo.userId.includes('@') &&
-    userInfo.userId.includes('.') &&
-    userInfo.userPw.includes.length >= 6;
+    userInfo.email.includes('@') &&
+    userInfo.email.includes('.') &&
+    userInfo.password.length >= 6;
   const navigate = useNavigate();
   const goToMain = () => {
     navigate('/');
@@ -34,28 +35,20 @@ const Login = ({ setModalOpen }) => {
   function validateUser(e) {
     e.preventDefault();
 
-    fetch('API주소', {
+    fetch('http://10.58.52.88:8002/users/login', {
       method: 'POST',
       headers: { 'Content-type': 'application/json;charset=utf-8' },
       body: JSON.stringify(userInfo),
     })
-      .then(response => {
-        if (response.ok === true) {
-          return response.json();
-        }
-        throw new Error('네트워크가 불안정합니다. 다시 시도 해 주세요');
-      })
-      .catch(error => console.log(error))
+      .then(response => response.json())
+
       .then(data => {
         if (data.message === 'login success') {
-          localStorage.setItem('TOKEN', data.token);
-          alert('로그인에 성공했습니다');
           navigate({ goToMain });
-        } else {
-          alert('아이디와 비밀번호를 확인 해 주세요');
         }
       });
   }
+
   return (
     <div className="login">
       <div className="loginScreen">
@@ -64,12 +57,13 @@ const Login = ({ setModalOpen }) => {
 
           <div className="emailWrapper">
             <input
-              name="userId"
+              name="email"
               className="loginEmail"
               onChange={getUserInfo}
               placeholder="이메일주소"
               type="text"
             />
+
             {!emailChk && (
               <p className="message">이메일 형식에 맞지 않습니다.</p>
             )}
@@ -77,7 +71,7 @@ const Login = ({ setModalOpen }) => {
           <div className="pwWrapper">
             <div className="containerPw">
               <input
-                name="userPw"
+                name="password"
                 className="loginPw"
                 onChange={getUserInfo}
                 type={showPw ? 'text' : 'password'}
@@ -104,7 +98,7 @@ const Login = ({ setModalOpen }) => {
 
           <div className="loginBtnWrapper">
             <button
-              onSubmit={validateUser}
+              onClick={validateUser}
               type="button"
               disabled={!isUserVaidate}
               className="loginBtn"
