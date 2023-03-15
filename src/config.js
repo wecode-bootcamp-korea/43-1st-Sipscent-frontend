e9de662c7e9312d7eac44cadfbc201c1f79b3bee
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-03-15 12:00:32 +0900

    MODIFY: MockData 수정 및 orderPage 리뷰 반영, ADD: config파일 생성 및 url 변수 작성

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const BASE_URL = 'http://';
+export const APIS = {
+  login: `${BASE_URL}/users/login`,
+  signup: `${BASE_URL}/users/signup`,
+  carts: `${BASE_URL}/carts`,
+  items: `${BASE_URL}/items`,
+  orders: `${BASE_URL}/orders`,
+};
--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { ORDER_INPUT_DATA } from './orderInputData';
+import { BASE_URL } from '../../config';
 import './Order.scss';
 
 const Order = () => {
+  const [orderProductData, setOrderProductData] = useState({});
   const [inputValue, setInputValue] = useState({
     userPhoneNumber: '',
     userAddress: '',
   });
-
-  const [orderProductData, setOrderProductData] = useState([]);
-
   const userPhoneNumberCheck = inputValue.userPhoneNumber.length > 3;
 
   const isShowHintText = _title =>
@@ -22,8 +21,16 @@ const Order = () => {
     setInputValue({ ...inputValue, [name]: value });
   }
 
+  useEffect(() => {
+    fetch('/data/orderData.json')
+      .then(res => res.json())
+      .then(data => {
+        setOrderProductData(data);
+      });
+  }, []);
+
   const handleSubmit = () => {
-    fetch('', {
+    fetch(`${BASE_URL}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
@@ -34,24 +41,21 @@ const Order = () => {
       .then(data => data);
   };
 
-  useEffect(() => {
-    fetch('/data/orderData.json')
-      .then(res => res.json())
-      .then(data => {
-        setOrderProductData([data]);
-      });
-  }, []);
   return (
     <div className="order">
       <div className="orderInfoContainer">
         <div className="orderUserInfoBox">
           <h1 className="orderHead">주문자 정보</h1>
-          {orderProductData.map(({ username, email }, item_id) => (
-            <div key={item_id} className="orderUserInfo">
-              <p className="orderUserInfoUserName">{username}</p>
-              <p className="orderUserInfoUserEmail">{email}</p>
-            </div>
-          ))}
+          <div className="orderUserInfo">
+            <p className="orderUserInfoUserName">
+              {orderProductData.orderList &&
+                orderProductData.orderList[0].username}
+            </p>
+            <p className="orderUserInfoUserEmail">
+              {orderProductData.orderList &&
+                orderProductData.orderList[0].email}
+            </p>
+          </div>
           <form className="orderUserInfoInputContainer">
             {ORDER_INPUT_DATA.map(({ title, helpText, name }, item_id) => (
               <div key={item_id} className="orderUserInfoInputBox">
@@ -61,7 +65,6 @@ const Order = () => {
                   className="orderUserInput"
                   name={name}
                 />
-
                 {isShowHintText(title) && (
                   <div className="orderUserInputHelpText">{helpText}</div>
                 )}
@@ -72,44 +75,49 @@ const Order = () => {
       </div>
       <div className="orderSummary">
         <h1 className="orderHead">제품 결제 요약</h1>
-        {orderProductData[0]?.orderList.map(
-          ({ itemname, quantity, price }, item_id) => (
-            <div key={item_id} className="orderSummaryProductList">
-              <div className="orderSummaryProduct">
-                <div>{itemname}</div>
-                <div>{quantity} 개</div>
+        {orderProductData.orderList &&
+          orderProductData.orderList.map(
+            ({ itemname, quantity, price }, item_id) => (
+              <div key={item_id} className="orderSummaryProductList">
+                <div className="orderSummaryProduct">
+                  <div>{itemname}</div>
+                  <div>{quantity} 개</div>
+                </div>
+                <div className="orderSummaryProductPrice">
+                  {Math.trunc(price)
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                  ₩
+                </div>
               </div>
-              <div className="orderSummaryProductPrice">
-                {Math.trunc(price)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                ₩
-              </div>
-            </div>
-          )
-        )}
-        {orderProductData.map(({ point, totalPrice }, key) => (
-          <div key={key} className="orderSummaryTotal">
-            <div className="orderSummaryPoint">
-              <div>사용가능한 포인트</div>
-              <div>
-                {Math.trunc(point)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                포인트
-              </div>
-            </div>
-            <div className="orderSummaryTotalPrice">
-              <strong>합계</strong>
-              <strong>
-                {Math.trunc(totalPrice)
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-                ₩
-              </strong>
+            )
+          )}
+        <div className="orderSummaryTotal">
+          <div className="orderSummaryPoint">
+            <div>사용가능한 포인트</div>
+            <div>
+              {Math.trunc(
+                orderProductData.orderList &&
+                  orderProductData.orderList[0].point
+              )
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              포인트
             </div>
           </div>
-        ))}
+          <div className="orderSummaryTotalPrice">
+            <strong>합계</strong>
+            <strong>
+              {Math.trunc(
+                orderProductData.orderList &&
+                  orderProductData.orderList[0].totalPrice
+              )
+                .toString()
+                .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+              ₩
+            </strong>
+          </div>
+        </div>
         <div className="orderButtonBox">
           <button onClick={handleSubmit} type="button" className="orderButton">
             결제하기
